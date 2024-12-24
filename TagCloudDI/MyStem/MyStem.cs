0aab977f5af79a8cb29041f1ccba31e0a19943da
SomEnaMeforme <rushkova909@gmail.com>
2024-12-24 05:39:12 +0500

    Add mysteam

---
+++ TagCloudDI/MyStem/MyStem.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace TagCloudDI.MyStem
+{
+    public static class MyStem
+    {
+        public static string AnalyseWords(string words)
+        {
+            var directory = ".\\MyStem";
+            var inputFile = Path.Combine(directory, "input.txt");
+            var outputFile = Path.Combine(directory, "output.txt");
+            var mySteam = Path.Combine(directory, "mystem.exe");
+            using (var writer = new StreamWriter(inputFile))
+            {
+                writer.Write(words);
+            }
+            var arguments = string.Format("-in {0} {1}", inputFile, outputFile);
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = mySteam,
+                    Arguments = arguments,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            using var reader = new StreamReader(outputFile, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+    }
+}
